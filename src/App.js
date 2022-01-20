91dc66405bd279c5a6a233bf1728cf427db4b6b9
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-20 11:59:43 +0100

    remove App.css and index from src folder and move in style folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Card } from './components/Card'
 import { Header } from './components/Header'
 import { Total } from './components/Total';
 
-import './App.css';
+import './style/App.css';
 
 function App() {
   return (
