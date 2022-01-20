6dbae98835a4159facedca984e59c391929abbd5
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-20 14:30:57 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { Card } from './components/Card'
 import { Header } from './components/Header'
-import { Total } from './components/Total';
+import { Card } from './components/Card'
 
 import './style/Banner.css';
+import './style/Card.css';
 
 function App() {
   return (
     <div>
       <Header />
       <Card />
-      <Total />
     </div>
   );  
 }
