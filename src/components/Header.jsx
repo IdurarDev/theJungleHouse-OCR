9f3309aa693147a78c5afc19fed60173b40c1539
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-20 12:44:19 +0100

    add img tag and style and import logo in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
+import logo from '../asset/logo.png'
+
 export const Header = () => {
-  const title = 'La maison jungle'
-  return (<h1 className="lmj-banner">{title}</h1>)
+  const title = 'La maison jungle';
+  return (<header className='lmj-banner'>
+    <img src={logo} alt="logo de la maison jungle" className='lmj-logo' />
+    <h1 className="lmj-title">{title}</h1>
+  </header>)
+  
 }
 
