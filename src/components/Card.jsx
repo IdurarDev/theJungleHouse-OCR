636aa6168a61f13ba346638311f2053c16743b2d
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-20 14:32:23 +0100

    modif Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,29 @@
+import { Fragment } from "react"
+import '../style/Card.css'
+
+let plantes = {
+  "nameOne": "Monstera",
+  "nameTwo": "Lierre",
+  "nameThree": "Bouquet de fleurs",
+  "priceOne" : 8,
+  "priceTwo" : 10,
+  "priceThree" : 15
+} 
+
 export const Card = () => {
   const title = 'mon panier';
+
   return (
-    <aside className="lmj-card">
-      <h2>{title}</h2>
-    </aside>
+    <Fragment> 
+      <aside className="lmj-card">
+        <h2>{title}</h2>
+        <ul>
+          <li>{`${plantes.nameOne}: ${plantes.priceOne} €`}</li>
+          <li>{`${plantes.nameTwo}: ${plantes.priceTwo} €`}</li>
+          <li>{`${plantes.nameThree}: ${plantes.priceThree} €`}</li>
+        </ul>
+        <p>Total du panier: {plantes.priceOne + plantes.priceTwo + plantes.priceThree} €</p> 
+      </aside>
+    </Fragment>
   )
 }
