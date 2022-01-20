00cc778dd3cda78a7b78dc2452cefa7e513fa844
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-20 14:52:58 +0100

    add ShoppingList component in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Card } from './components/Card'
 
 import './style/Banner.css';
 import './style/Card.css';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div>
       <Header />
       <Card />
+      <ShoppingList />
     </div>
   );  
 }
