fe7f5457c0606f10eada27880ba800f16cb3e4c2
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-21 10:56:57 +0100

    add function which listing categories of plant and names of plant

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,19 +1,24 @@
-const plantList = [
-  'monstera',
-  'ficus lyrata',
-  'pothos argenté',
-  'yucca',
-  'palmier'
-]
+import { plantList } from "../datas/plantList";
 
-function ShoppingList() {
+export function ShoppingList() {
+  const categories = plantList.reduce(
+    (acc, plant) => 
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+      []
+  )
   return (
-    <ul>
-      {plantList.map((plant, index) => (
-          <li key={`${plant}-${index}`}>{ plant }</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{ cat }</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+            <li key={plant.id}>{ plant.name }</li>
+        ))}
+      </ul>
+    </div>
+    
   )
 }
-
-export default ShoppingList
