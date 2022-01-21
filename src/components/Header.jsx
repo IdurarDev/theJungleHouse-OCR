a0585c9fdafabfd38f6142b1ff05ff5f3f81017e
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-21 15:55:53 +0100

    modif Header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,6 @@
-import logo from '../asset/logo.png'
+import '../style/Banner.css'
 
-export const Header = () => {
-  const title = 'La maison jungle';
-  return (<header className='lmj-banner'>
-    <img src={logo} alt="logo de la maison jungle" className='lmj-logo' />
-    <h1 className="lmj-title">{title}</h1>
-  </header>)
-  
+export const Header = ({children}) => {
+  return (<header className='lmj-banner'>{children}</header>)
 }
 
