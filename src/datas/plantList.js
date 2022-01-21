8e1c12ba8e67facff5063392b338cf60ba2bbdac
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-21 15:53:30 +0100

    update plantList.js

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -2,49 +2,67 @@ export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
-		id: '1ed'
+		id: '1ed',
+    light:2,
+    water:3
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
-		id: '2ab'
+		id: '2ab',
+    light:3,
+    water:1
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-    isSpecialOffer: true
+    isSpecialOffer: true,
+    light:1,
+    water:2
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
-		id: '4kk'
+		id: '4kk',
+    light:3,
+    water:1
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
-		id: '5pl'
+		id: '5pl',
+    light:3,
+    water:1
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo'
+		id: '6uo',
+    light:2,
+    water:2
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-    isSpecialOffer: true
+    isSpecialOffer: true,
+    light:2,
+    water:3
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-    isSpecialOffer: true
+    isSpecialOffer: true,
+    light:2,
+    water:1
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+    light:2,
+    water:1
 	}
 ]
