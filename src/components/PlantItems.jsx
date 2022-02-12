804c684a169b27e8fdfb1c9fa1af4d1bd307a402
Hamid Berkaine <hamiddev78@gmail.com>
2022-02-12 18:56:31 +0100

    create plantItems file

--- src/components/PlantItems.jsx
+++ src/components/PlantItems.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { CareScale } from './CareScale';
+import { plantList } from '../datas/plantList';
 
 export function PlantItems(props) {
   return (
-    <div>
-
+    <div className='lmj-plant-item-cover'>
+      <p>{props.name}</p>
+      <img src={props.light} alt="a sun" />
+      <img src={props.water} alt="a water" />
+      <CareScale careType='light' scaleValue={plantList.light} />
+      <CareScale careType='water' scaleValue={plantList.water} />
     </div>
   )
 }
