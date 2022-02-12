e8743bde25e4df2fa689e7be78048b4bf023d5c2
Hamid Berkaine <hamiddev78@gmail.com>
2022-02-12 18:57:41 +0100

    update App file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { Header } from './components/Header'
-import { Card } from './components/Card'
+import {Header} from './components/Header'
+import logo from './assets/logo.png'
+// import {Card} from './components/Card'
+import {ShoppingList} from './components/ShoppingList';
 
 import './style/Banner.css';
-import './style/Card.css';
-import ShoppingList from './components/ShoppingList';
+// import './style/Card.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Card />
+      <Header>
+        <img src={logo} alt="logo de la maison jungle" className='lmj-logo' />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Header>
+      {/* <Card /> */}
       <ShoppingList />
     </div>
   );  
